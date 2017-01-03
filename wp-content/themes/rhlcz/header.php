af1261f09536de1128d5d55bd4d3dc313ccc23ce
Leah <leah.li@bluecomgroup.com>
2017-01-03 17:32:26 +0800

    line height updated

--- wp-content/themes/rhlcz/header.php
+++ wp-content/themes/rhlcz/header.php
@@ -29,7 +29,8 @@
     <?php do_action( '__before_page_wrapper' ); ?>
 
     <div id="tc-page-wrap" class="<?php echo implode( " ", apply_filters('tc_page_wrap_class', array() ) ) ?>">
-    
+    <div class="bdsharebuttonbox"><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"2","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 
   		<?php do_action( '__before_header' ); ?>
 
